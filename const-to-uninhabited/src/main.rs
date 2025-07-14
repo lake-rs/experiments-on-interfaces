f4cb4807bd93cd561c116339e9c483836c0ce3ab
chrysn <chrysn@fsfe.org>
2025-07-14 17:58:38 +0200

    show how a maybe-const type can work

---
+++ const-to-uninhabited/src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Copy, Clone)]
+struct MaybeUninhabited<const UNINHABITED: usize> {
+    // ZST or uninhabited
+    _field: [core::convert::Infallible; UNINHABITED],
+}
+
+impl<const UNINHABITED: usize> MaybeUninhabited<UNINHABITED> {
+    const fn new() -> Self {
+        const {
+            if UNINHABITED == 0 {
+                assert!(core::mem::size_of::<Self>() == 0);
+                // Just going through &[].try_into().unwrap() doesn't cut it b/c that's still only
+                // present for finitely many arrays
+                //
+                // This is through unsafe, but the assert above makes it pretty obvious that it is
+                // safe here.
+                unsafe { core::mem::zeroed() }
+            } else {
+                panic!("This is uninhabited")
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("Hello");
+    let _a: MaybeUninhabited<0> = MaybeUninhabited::new();
+    //let _b: MaybeUninhabited<1> = MaybeUninhabited::new();
+}
